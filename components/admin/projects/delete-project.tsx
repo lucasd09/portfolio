81ecd876a84633f4d6d81e6d162e8b90241db50b
lucas.dalan <lucas.dalan@frgnet.com.br>
2024-03-01 17:55:47 -0300

    (feat) - delete project dialog

---
+++ components/admin/projects/delete-project.tsx
@@ -0,0 +1,46 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "../../ui/alert-dialog";
+import { TrashIcon } from "lucide-react";
+import { Button } from "../../ui/button";
+import { toast } from "sonner";
+
+export default function DeleteProject({ id }: { id: number }) {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button variant="outline" className="h-8 w-8 p-0">
+          <TrashIcon className="h-4 w-4" />
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Confirmação</AlertDialogTitle>
+          <AlertDialogDescription>
+            Deseja deletar esse projeto?
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancelar</AlertDialogCancel>
+          <AlertDialogAction
+            onClick={() => {
+              return toast("Sucesso", {
+                description: "Projeto removido com êxito.",
+              });
+            }}
+          >
+            Continuar
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
